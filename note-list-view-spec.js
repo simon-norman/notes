777f7e257e0e8057eea76d7974d2195ae5ac976b
Soroush Khosravi <sorush.kh68@gmail.com>
2019-03-18 16:43:31 +0000

    The html string is ok now.

--- note-list-view-spec.js
+++ note-list-view-spec.js
@@ -6,7 +6,26 @@ function listNoteView() {
   }
   var noteListView = new NoteListView(NoteListDouble);
   var viewString = noteListView.getNoteString();
-  assert.isTrue(viewString === "")
+  assert.isTrue(viewString === "<ul></ul>")
 }
 
 listNoteView();
+
+function testReturnsHtmlWithOneNote() {
+  var noteDouble = {
+    showText: function(){
+      return "abc";
+    }
+  }
+
+  var NoteListDouble = {
+    getNotes: function(){
+      return [noteDouble];
+    }
+  }
+
+  var noteListView = new NoteListView(NoteListDouble);
+  var viewString = noteListView.getNoteString();
+  assert.isTrue(viewString === "<ul><li><div>abc</div></li></ul>")
+}
+testReturnsHtmlWithOneNote()
--- note-list-view.js
+++ note-list-view.js
@@ -3,7 +3,12 @@
     this.NoteList = NoteList ;
   }
   NoteListView.prototype.getNoteString = function(){
-    return ""
+    var emptyString = "<ul>"
+    this.NoteList.getNotes().forEach(function(element) {
+      emptyString = emptyString+"<li><div>"+element.showText() + "</div></li>"
+    })
+    emptyString =emptyString+ "</ul>";
+    return emptyString;
   }
   exports.NoteListView = NoteListView;
 }) (this);
